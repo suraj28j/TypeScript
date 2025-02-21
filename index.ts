a21f0e4e795bfbd3caae0294becd3017d24fe711
Suraj Kumar <suraj28j@gmail.com>
2025-02-21 22:33:33 +0530

    update

--- index.ts
+++ index.ts
@@ -1,10 +1,45 @@
+// [ Basics ] //
+
 // var num:number = 28;
 // num = "suraj" // not allow
 // console.log(num);
 
+// function sum(a:number,b:number){   // type annotation
+//     return a+b;
+// }
+// // sum(7,"R") // not allow
+// console.log(sum(8,6));
 
-function sum(a:number,b:number){   // type annotation
-    return a+b;
-}
-// sum(7,"R") // not allow
-console.log(sum(8,6));
+// let myNum = 28;
+// console.log(myNum.toString());
+
+// ---------------------------------------------------------------- //
+
+// [ number data types ] //
+// number type represent numeric values, including integers and floating-point numbers //
+
+// let myAge:number = 35;
+// let myName = "Suraj"
+// let result:number = myAge+myName // not allow
+// let result:number = 120;
+
+// let val:number = Math.sqrt(16)
+
+
+// ---------------------------------------------------------------- //
+
+// [ string data types ] //
+
+let firstName = "Suraj";
+let lastName = "Kumar";
+// let myName = firstName+lastName;
+
+let text: string = "My name is Suraj Kumar";
+let textLowerCase: string = text.toLowerCase();
+let textUpperCase: string = text.toUpperCase();
+// console.log(textLowerCase, textUpperCase);
+
+
+// ---------------------------------------------------------------- //
+
+// [ boolean and bigInt data types ] //
---
+++ test1.ts
@@ -0,0 +1,5 @@
+// function sum(a){
+//     return a++;
+// }
+
+// console.log(sum(5));
