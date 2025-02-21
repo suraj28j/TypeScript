e227db787d1445ffe85d84a7ea233c0714bd9f23
Suraj Kumar <suraj28j@gmail.com>
2025-02-21 15:23:38 +0530

    update

---
+++ index.ts
@@ -0,0 +1,10 @@
+// var num:number = 28;
+// num = "suraj" // not allow
+// console.log(num);
+
+
+function sum(a:number,b:number){   // type annotation
+    return a+b;
+}
+// sum(7,"R") // not allow
+console.log(sum(8,6));
