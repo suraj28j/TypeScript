95b1f27c1d20dbbd74c63eaa4fce54e4c2d69104
Suraj Kumar <suraj28j@gmail.com>
2025-03-01 16:40:20 +0530

    update generic

--- Basics/generic.ts
+++ Basics/generic.ts
@@ -13,14 +13,35 @@
 // const booleanResult: number | string | boolean = logAndReturn(true);
 
 
+// Example [1]
+// function logAndReturn<T>(value: T): T {
+//     return value;
+// }
 
-function logAndReturn<T>(value: T): T {
-    return value;
+// const numberResult = logAndReturn(45);
+// // const numberResult = logAndReturn<number>(45);
+// const stringResult = logAndReturn("Suraj");
+// const booleanResult = logAndReturn(true);
+// console.log(numberResult);
+
+
+// Example [2]
+
+// function add<T, U>(a: T, b: U) {
+//     console.log(typeof a);
+//     console.log(typeof b);
+// }
+
+// const result1 = add<string, number>("Sk", 28)
+// const result2 = add<string, string>("Suraj", "Kumar")
+
+
+// Example [3]
+
+function fun<T, U>(a: T, b: U, c: boolean) {
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
 }
 
-const numberResult = logAndReturn(45);
-// const numberResult = logAndReturn<number>(45);
-const stringResult = logAndReturn("Suraj");
-const booleanResult = logAndReturn(true);
-
-console.log(numberResult);
+const result3 = fun<string, number>("Sk", 28, true)
