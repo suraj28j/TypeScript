dc161a28d75587179b7c859624b3c3bba2089296
Suraj Kumar <suraj28j@gmail.com>
2025-03-01 22:32:30 +0530

    file update

---
+++ ProjectConfig/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const user1 = {
+    name: "Ramadhir Singh",
+    age: 75
+};
+let res = user1.name;
+console.log(res);
---
+++ ProjectConfig/src/index.ts
@@ -0,0 +1,12 @@
+type User = {
+    name: string;
+    age: number
+}
+
+const user1: User = {
+    name: "Ramadhir Singh",
+    age: 75
+}
+
+let res  = user1.name;
+console.log(res);
